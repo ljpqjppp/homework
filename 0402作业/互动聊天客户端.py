5c3423188a4b3248af45a811ea24494aecbea6c4
刘继鹏 <liujipeng@liujipengs-MacBook-Pro.local>
2019-04-07 10:01:13 +0800

    222

--- 0402作业/互动聊天客户端.py
+++ 0402作业/互动聊天客户端.py
@@ -1,14 +1,34 @@
 import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_addr = ('127.0.0.1', 41903)
-client.connect(server_addr)
-while 1:
-    try:
-        msg = input('客户端说:')
-        client.send(msg.encode())
-        msg = client.recv(65535)
-        print('服务器说:', msg.decode())
-    except Exception:
-        break
-client.close()
-print('链接已经完成')
+from threading import Thread
+ss = socket.socket()
+ss.connect(('127.0.0.1',9520))
+
+
+def conn_send(ss):
+    while 1:
+        msg = input('请输入消息:')
+        try:
+            ss.send(msg.encode())
+        except Exception:
+            break
+    print('发送程序已经结束')
+
+def conn_recv(ss):
+    while 1:
+        return_msg = ss.recv(65535)
+        if not return_msg:
+            break
+        print(return_msg.decode())
+    print('     ')
+    print('接收程序已经结束')
+
+
+t1 = Thread(target=conn_send,args=(ss,))
+t2 = Thread(target=conn_recv,args=(ss,))
+
+t1.start()
+t2.start()
+print('聊天客户端已经启动')
+t1.join()
+t2.join()
+print('聊天客户端已经结束')
